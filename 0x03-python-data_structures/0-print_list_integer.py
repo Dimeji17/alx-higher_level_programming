226642011f3959e3a4e1b2332fbe60c44800607e
Dimeji17 <oladimejialhassan17@gmail.com>
2022-08-27 12:11:16 -0700

    0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,4 +2,4 @@
 def print_list_integer(my_list=[]):
     for x in range(len(my_list)):
         if type(x) == int:
-            print("{}".format(len(my_list[1])))
+            print("{}".format(len(my_list[x])))
