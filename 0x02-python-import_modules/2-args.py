f97fc6b1064a2c409d251362725a810303c7aba9
Dimeji17 <oladimejialhassan17@gmail.com>
2022-08-29 11:13:28 -0700

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+# !/usr/bin/python3
+if __name__ == "__main__":
+
+    from sys import argv
+
+    length = 0
+
+    for count in range(0, len(argv)):
+        if (count == 0):
+            print("{} arguments".format(count)))
+
+        elif (count == 1):
+            print("{}".format(count))
+
+        else:
+            print("{}".format(count))
+
